67f05c925a26c4fb0776d8172b9c9bcce72a85ae
プラジュワル ニンマガッダ <prajcindav@outlook.com>
2017-08-20 17:57:16 -0300

    Add project files.

---
+++ My EMGU Program/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace My_EMGU_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                //Load the Image
+                Image<Bgr, Byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
+
+                //Display the Image
+                pictureBox1.Image = My_Image.ToBitmap();
+            }
+        }
+    }
+}
